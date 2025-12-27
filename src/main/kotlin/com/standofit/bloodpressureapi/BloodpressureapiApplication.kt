9235a0ec84a56e31fb93d94e67f899420fd86d64
RenzoRC99 <renzoromero99@hotmail.com>
2025-12-27 22:04:32 +0100

    primer commit

---
+++ src/main/kotlin/com/standofit/bloodpressureapi/BloodpressureapiApplication.kt
@@ -0,0 +1,11 @@
+package com.standofit.bloodpressureapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BloodpressureapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BloodpressureapiApplication>(*args)
+}
